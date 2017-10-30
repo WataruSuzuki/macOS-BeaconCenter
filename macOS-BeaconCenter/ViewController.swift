d3780c7cb7d13558e6892bf6557208e0dc2408ea
WataruSuzuki <wataru0406@gmail.com>
2017-10-30 21:30:00 +0900

    Created Xcode project

---
+++ macOS-BeaconCenter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS-BeaconCenter
+//
+//  Created by 鈴木航 on 2017/10/30.
+//  Copyright © 2017年 WataruSuzuki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
